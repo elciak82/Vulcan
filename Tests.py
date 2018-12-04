45ef512b6ff606d1c1d3ccc324b7c45b2e00afc6
elciak82 <33999374+elciak82@users.noreply.github.com>
2018-12-04 14:51:55 +0100

    add a bug to test

--- Tests.py
+++ Tests.py
@@ -10,7 +10,8 @@ from selenium import webdriver
 
 class TestCase(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome(); 
+#         add a bug
         self.driver.maximize_window()
 
     def tearDown(self):
